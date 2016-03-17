8a0db335f6e0f4f4d4bdf7286d0faf440ead86e1
Laurent Raufaste <analogue@glop.org>
2016-03-17 16:45:43 -0400

    debug mode

--- update_loan_offers.py
+++ update_loan_offers.py
@@ -18,6 +18,7 @@ from poloniex import Poloniex
 
 BTC_PER_BIG_OFFER = 10.0
 DEBUG = False
+LOAN_DURATION = 2
 
 
 def main():
@@ -39,21 +40,30 @@ def main():
     poloniex = Poloniex(api_key=api_key, secret=secret)
     unused_btc = poloniex.get_unused()
 
+    if DEBUG:
+        print "Unused BTC:", unused_btc
+
     if unused_btc:
         min_big_offer = poloniex.get_min_offer_rate(BTC_PER_BIG_OFFER)
 
         # Get 0.0001% below
         my_rate = min_big_offer - (0.0001 / 100)
 
-        # Place the actual order
-        order_id = poloniex.offer_btc_loan(my_rate, unused_btc, 2)
-
-        # Order status
-        if order_id:
-            print "Order placed ({})".format(order_id),
+        if DEBUG:
+            print "poloniex.offer_btc_loan({}, {}, {})".format(
+                my_rate, unused_btc, LOAN_DURATION)
         else:
-            print "Order failed",
-        print "(Rate:", my_rate * 100, "%, Amount:", unused_btc, "BTC)"
+            # Place the actual order
+            order_id = poloniex.offer_btc_loan(my_rate,
+                                               unused_btc,
+                                               LOAN_DURATION)
+
+            # Order status
+            if order_id:
+                print "Order placed ({})".format(order_id),
+            else:
+                print "Order failed",
+            print "(Rate:", my_rate * 100, "%, Amount:", unused_btc, "BTC)"
 
 if __name__ == "__main__":
     main()
